fee77478fa09dd65b69f4928722ed90d3807fcbe
PrinceTG80 <tgavkhare80@gmail.com>
2021-08-01 13:00:11 +0530

    check if deploying on heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/IONIC-TGPLATFORM-APP'));
+app.use(express.static('./dist/ionictgplatformapp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/IONIC-TGPLATFORM-APP'}),
+    res.sendFile('index.html', {root: 'dist/ionictgplatformapp'}),
 );
 
 // Start the app by listening on the default Heroku port
